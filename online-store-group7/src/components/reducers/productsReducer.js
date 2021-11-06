12f49a0cc846a7f4b317e8c4ac120b1032276562
Nasser-96 <nasserabunasir96@outlook.com>
2021-11-06 13:16:23 +0300

    first commit

---
+++ online-store-group7/src/components/reducers/productsReducer.js
@@ -0,0 +1,16 @@
+import { Products } from "../objectsOfProducts";
+
+const initialState ={
+    products:Products,
+}
+
+const ProductsReducer = (state = initialState,{type,payload})=>{
+    switch (type){
+    
+        default:
+            return state;
+    }
+        
+};
+
+export default ProductsReducer
