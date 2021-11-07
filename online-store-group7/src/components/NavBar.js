7b7b70475e74a231c57f3cd7d6ec69d2cecd2f6c
Nasser-96 <nasserabunasir96@outlook.com>
2021-11-07 13:40:23 +0300

    change image file

--- online-store-group7/src/components/NavBar.js
+++ online-store-group7/src/components/NavBar.js
@@ -1,4 +1,4 @@
-
+import cart from '../Images/cart.png'
 
 function NavBar (){
 
@@ -8,7 +8,7 @@ function NavBar (){
     {/* <BrowserRouter> */}
        <li className="listl listlA" >Home </li>
        <li className="listl listlA" >Wish Item</li>
-       <li className="listR listlA " > <img src="https://cdn-icons.flaticon.com/png/512/4647/premium/4647563.png?token=exp=1636274021~hmac=cd0e3610d429b309ba2f383126b39cbb" className="imgCart"/></li>
+       <li className="listR listlA " > <img src={cart} className="imgCart"/></li>
        <li className="listR listlA ">Sign in</li>
        <li className="listR listlA ">Search</li>
        <li className="listR listlA" ><input type="text"  className="textNav"/></li>
