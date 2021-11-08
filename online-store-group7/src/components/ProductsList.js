43c1b8ac576f6f628b52a2af22d3e5299cfc8a42
Shrooq713 <shroog713@gmail.com>
2021-11-08 11:38:02 +0300

    “Pagination”

--- online-store-group7/src/components/ProductsList.js
+++ online-store-group7/src/components/ProductsList.js
@@ -1,27 +1,39 @@
 import { useSelector } from "react-redux";
 import "../styleFiles/productsList.css"
+import { useState } from "react";
+import Product from "./Product";
+import Pagination from "./Pagination";
 function ProductsList() {
     const state = useSelector((state)=>{
         return {
           product: state.ProductsReducer.products,
         }
       })
+    //pagination start from page 1
+    const [currentPage, setCurrentPage] = useState(1);
+    // how many post inside each page
+    const [productPerPage,setProductPerPage] = useState(16);
+
+    // Get current posts
+    //                      1*10=10  > 2*10=20  >3*10=30
+    const indexOfLastPost = currentPage * productPerPage;
+    //                      10-10=0  > 20-10=10
+    const indexOfFirstPost = indexOfLastPost - productPerPage;
+    // returns a copy of a portion of an array into a new array object 
+      // selected from start to end ( end not included)
+    const currentProduct = state.product.slice(indexOfFirstPost, indexOfLastPost);
+    // Change page based on clicking number from pagination file
+
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return (
         <div>
-            <div className="cardPr">
-
-{state.product.map(e=>{
-  
-  return(
-  <div>
-  <img src={e.imgPr} className="imgSize"/>
-  <p>{e.namePr}</p>
-  </div>
-  )
-})}
-
-</div>
+            <Product products={currentProduct}/>
+            <Pagination
+              productPerPage={productPerPage}
+              totalProducts={state.product.length}
+              paginate={paginate}
+            />
         </div>
     );
 }
